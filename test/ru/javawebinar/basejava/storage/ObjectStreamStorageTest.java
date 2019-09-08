45164c8fe9903f88afdaa9dcff1010cbf376c9e3
Alexey <alekseiovechkin@gmail.com>
2019-09-08 11:38:04 +0300

    HW09 implemented SerializableStrategy

--- test/ru/javawebinar/basejava/storage/ObjectStreamStorageTest.java
+++ test/ru/javawebinar/basejava/storage/ObjectStreamStorageTest.java
@@ -1,10 +1,7 @@
 package ru.javawebinar.basejava.storage;
 
-import static org.junit.Assert.*;
-import static ru.javawebinar.basejava.storage.AbstractStorageTest.STORAGE_DIR;
-
 public class ObjectStreamStorageTest extends AbstractStorageTest {
     public ObjectStreamStorageTest() {
-        super(new ObjectStreamStorage(STORAGE_DIR));
+        super(new ObjectStreamFileStorage(STORAGE_DIR));
     }
 }
