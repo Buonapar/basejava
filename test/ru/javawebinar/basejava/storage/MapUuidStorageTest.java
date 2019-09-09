d15e0793420997b6fdc6a904fb547345cd3a9faa
Alexey <alekseiovechkin@gmail.com>
2019-09-09 14:21:26 +0300

    HW09 corrected1

--- test/ru/javawebinar/basejava/storage/MapUuidStorageTest.java
+++ test/ru/javawebinar/basejava/storage/MapUuidStorageTest.java
@@ -1,17 +1,8 @@
 package ru.javawebinar.basejava.storage;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 public class MapUuidStorageTest extends AbstractStorageTest{
 
     public MapUuidStorageTest() {
         super(new MapUuidStorage());
-    }
-
-    @Override
-    @Test
-    public void getAll() {
-        Assert.assertEquals(3, storage.size());
     }
 }
