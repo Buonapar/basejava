5dffd38ebebda6f9b6f3bdf714c88119da541a48
Alexey <alekseiovechkin@gmail.com>
2019-09-18 18:36:40 +0300

    HW10 corrected4

--- src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
+++ src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
@@ -11,28 +11,18 @@ import java.util.Objects;
 
 public class DataStreamSerializer implements StreamSerializer {
 
-    private <T> void writeWithExeption(Collection<T> collect, DataOutputStream dataOutputStream, WriteWithExeption<T> action) throws IOException {
-        Objects.requireNonNull(collect);
-        Objects.requireNonNull(dataOutputStream);
-        Objects.requireNonNull(action);
-        dataOutputStream.writeInt(collect.size());
-        for (T t : collect) {
-            action.write(t);
-        }
-    }
-
     @Override
     public void doWrite(Resume resume, OutputStream os) throws IOException {
         try (DataOutputStream dataOutputStream = new DataOutputStream(os)) {
             dataOutputStream.writeUTF(resume.getUuid());
             dataOutputStream.writeUTF(resume.getFullName());
 
-            writeWithExeption(resume.getContact().entrySet(), dataOutputStream, contact -> {
+            writeWithException(resume.getContact().entrySet(), dataOutputStream, contact -> {
                 dataOutputStream.writeUTF(contact.getKey().name());
                 dataOutputStream.writeUTF(contact.getValue());
             });
 
-            writeWithExeption(resume.getSection().entrySet(), dataOutputStream, sectionEntry -> {
+            writeWithException(resume.getSection().entrySet(), dataOutputStream, sectionEntry -> {
                 Section section = sectionEntry.getValue();
                 SectionType sectionType = SectionType.valueOf(sectionEntry.getKey().name());
                 dataOutputStream.writeUTF(sectionType.name());
@@ -43,14 +33,14 @@ public class DataStreamSerializer implements StreamSerializer {
                         break;
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
-                        writeWithExeption(((TextListSection) section).get(), dataOutputStream, dataOutputStream::writeUTF);
+                        writeWithException(((TextListSection) section).get(), dataOutputStream, dataOutputStream::writeUTF);
                         break;
                     case EDUCATION:
                     case EXPERIENCE:
-                        writeWithExeption(((CompanySection) section).get(), dataOutputStream, company -> {
+                        writeWithException(((CompanySection) section).get(), dataOutputStream, company -> {
                             dataOutputStream.writeUTF(company.getHomepage().getName());
                             dataOutputStream.writeUTF(company.getHomepage().getUrl());
-                            writeWithExeption(company.getPositions(), dataOutputStream, position -> {
+                            writeWithException(company.getPositions(), dataOutputStream, position -> {
                                 dataOutputStream.writeUTF(position.getStartDate().toString());
                                 dataOutputStream.writeUTF(position.getEndDate().toString());
                                 dataOutputStream.writeUTF(position.getTitle());
@@ -69,14 +59,9 @@ public class DataStreamSerializer implements StreamSerializer {
             String uuid = dataInputStream.readUTF();
             String fullName = dataInputStream.readUTF();
             Resume resume = new Resume(uuid, fullName);
-            int size = dataInputStream.readInt();
-            for (int i = 0; i < size; i++) {
-                resume.addContact(ContactType.valueOf(dataInputStream.readUTF()), dataInputStream.readUTF());
-            }
-            size = dataInputStream.readInt();
-            for (int i = 0; i < size; i++) {
+            listWalk(dataInputStream, ()-> resume.addContact(ContactType.valueOf(dataInputStream.readUTF()), dataInputStream.readUTF()));
+            listWalk(dataInputStream, ()-> {
                 SectionType sectionType = SectionType.valueOf(dataInputStream.readUTF());
-                List<Company> companies = new ArrayList<>();
                 switch (sectionType) {
                     case OBJECTIVE:
                     case PERSONAL:
@@ -84,39 +69,73 @@ public class DataStreamSerializer implements StreamSerializer {
                         break;
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
-                        int countDescriptions = dataInputStream.readInt();
-                        List<String> descriptions = new ArrayList<>();
-                        for (int j = 0; j < countDescriptions; j++) {
-                            descriptions.add(dataInputStream.readUTF());
-                        }
-                        resume.addSection(sectionType, new TextListSection(descriptions));
+                        resume.addSection(sectionType, new TextListSection(readWithException(dataInputStream, dataInputStream::readUTF)));
                         break;
                     case EDUCATION:
                     case EXPERIENCE:
-                        int countCompanies = dataInputStream.readInt();
-                        for (int j = 0; j < countCompanies; j++) {
-                            Link link = new Link(dataInputStream.readUTF(), dataInputStream.readUTF());
-                            List<Company.Position> positions = new ArrayList<>();
-                            int countPositions = dataInputStream.readInt();
-                            for (int k = 0; k < countPositions; k++) {
-                                Company.Position position = new Company.Position(YearMonth.parse(dataInputStream.readUTF())
-                                        , YearMonth.parse(dataInputStream.readUTF())
-                                        , dataInputStream.readUTF(), dataInputStream.readUTF());
-                                positions.add(position);
-                            }
-                            companies.add(new Company(link, positions));
-                        }
-                        resume.addSection(sectionType, new CompanySection(companies));
+                        resume.addSection(sectionType, new CompanySection(
+                                readWithException(dataInputStream, () -> new Company(
+                                        new Link(dataInputStream.readUTF(), dataInputStream.readUTF())
+                                        , readWithException(dataInputStream, () -> new Company.Position(
+                                        parseDate(dataInputStream)
+                                        , parseDate(dataInputStream)
+                                        , dataInputStream.readUTF()
+                                        , dataInputStream.readUTF()))))));
                         break;
                 }
-            }
+            });
             return resume;
         }
     }
 
+    private <T> void writeWithException(Collection<T> collect, DataOutputStream dataOutputStream, ConsumerWrite<T> action) throws IOException {
+        Objects.requireNonNull(collect);
+        Objects.requireNonNull(dataOutputStream);
+        Objects.requireNonNull(action);
+        dataOutputStream.writeInt(collect.size());
+        for (T t : collect) {
+            action.write(t);
+        }
+    }
+
+    private <T> List<T> readWithException(DataInputStream dataInputStream, ConsumerRead<T> action) throws IOException {
+        Objects.requireNonNull(dataInputStream);
+        Objects.requireNonNull(action);
+        List<T> ConsumerList = new ArrayList<>();
+        int size = dataInputStream.readInt();
+        for (int i = 0; i < size; i++) {
+            ConsumerList.add(action.read());
+        }
+        return ConsumerList;
+    }
+
+    private void listWalk(DataInputStream dataInputStream, ConsumerAdd action) throws IOException {
+        Objects.requireNonNull(dataInputStream);
+        Objects.requireNonNull(action);
+        int size = dataInputStream.readInt();
+        for (int i = 0; i < size; i++) {
+            action.add();
+        }
+    }
+
+    private YearMonth parseDate(DataInputStream dataInputStream) throws IOException {
+        return YearMonth.parse(dataInputStream.readUTF());
+    }
+
     @FunctionalInterface
-    private interface WriteWithExeption<T> {
+    private interface ConsumerWrite<T> {
+
         void write(T t) throws IOException;
+    }
+
+    @FunctionalInterface
+    private interface ConsumerRead<T> {
+        T read() throws IOException;
+    }
+
+    @FunctionalInterface
+    private interface ConsumerAdd {
+        void add() throws IOException;
     }
 
 
