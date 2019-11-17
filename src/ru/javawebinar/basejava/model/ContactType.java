8edf4565aa19a4d239a539838fd56d7d34f84cd5
Alexey <alekseiovechkin@gmail.com>
2019-11-17 23:07:54 +0300

    Lesson16 crud

--- src/ru/javawebinar/basejava/model/ContactType.java
+++ src/ru/javawebinar/basejava/model/ContactType.java
@@ -4,12 +4,42 @@ public enum ContactType {
     PHONE("Номер телефона"),
     MOBILE("Мобильный"),
     HOME_PHONE("Домашний тел."),
-    EMAIL("Электронная почта"),
-    SKYPE("Логин Skype"),
-    LINKEDIN("Профиль в LinkedIn"),
-    GITHUB("Профиль GitHub"),
-    STATCKOVERFLOW("Профиль Stackoverflow"),
-    HOME_PAGE("Домашняя страница");
+    EMAIL("Электронная почта") {
+        @Override
+        public String toHtml0(String value) {
+            return getTitle() + ": " + toLink("mailto:" + value, value);
+        }
+    },
+    SKYPE("Логин Skype") {
+        @Override
+        public String toHtml0(String value) {
+            return getTitle() + ": " + toLink("skype:" + value, value);
+        }
+    },
+    LINKEDIN("Профиль в LinkedIn") {
+        @Override
+        protected String toHtml0(String value) {
+            return toLink(value);
+        }
+    },
+    GITHUB("Профиль GitHub") {
+        @Override
+        protected String toHtml0(String value) {
+            return toLink(value);
+        }
+    },
+    STATCKOVERFLOW("Профиль Stackoverflow") {
+        @Override
+        protected String toHtml0(String value) {
+            return toLink(value);
+        }
+    },
+    HOME_PAGE("Домашняя страница") {
+        @Override
+        protected String toHtml0(String value) {
+            return toLink(value);
+        }
+    };
 
     private String title;
 
@@ -19,5 +49,21 @@ public enum ContactType {
 
     public String getTitle() {
         return title;
+    }
+
+    protected String toHtml0(String value) {
+            return title + ": " + value;
+    }
+
+    public String toHtml(String value) {
+        return (value == null) ? "": toHtml0(value);
+    }
+
+    public String toLink(String href) {
+        return toLink(href, title);
+    }
+
+    public static String toLink(String href, String title) {
+        return "<a href='" + href + "'>" + title + "</a>";
     }
 }
